6fb2357239d1ca89132342b605fbd0c52d2c4ff0
Lory Hou <lory.hou@augmentum.com>
2017-04-06 15:41:40 +0800

    add code to handle spring session

--- src/main/java/com/example/ResourceApplication.java
+++ src/main/java/com/example/ResourceApplication.java
@@ -4,16 +4,26 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-class ResourceApplication {
+class ResourceApplication extends WebSecurityConfigurerAdapter {
 
+	@Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http.cors().and().authorizeRequests()
+	      .anyRequest().authenticated();
+	  }
+	
 	@RequestMapping("/")
-	@CrossOrigin(origins="*", maxAge=3600)
+	@CrossOrigin(origins="*", maxAge=3600, allowedHeaders={"x-auth-token", "x-requested-with"})
 	public Message home() {
 		return new Message("Hello World");
 	}
@@ -21,6 +31,12 @@ class ResourceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceApplication.class, args);
 	}
+	
+	 @Bean
+	  HeaderHttpSessionStrategy sessionStrategy() {
+	    return new HeaderHttpSessionStrategy();
+	  }
+
 
 }
 
